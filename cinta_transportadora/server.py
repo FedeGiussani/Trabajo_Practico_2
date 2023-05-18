25b0af49aa9b1aa7162a6d97b3c6996a6a08069a
fatistrada <fatimastrada15@hotmail.com>
2023-05-18 20:48:32 -0300

    act

--- cinta_transportadora/server.py
+++ cinta_transportadora/server.py
@@ -1,10 +1,17 @@
 from flask import Flask, app, render_template
+from modules.cintaTransportadora import CintaTransportadora 
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def raiz():
-          
+    
+
+
+    #if request.method == 'POST':
+     #   N= int(request.form.get("N"))
+
+        
     return render_template("home.html") 
 
 
